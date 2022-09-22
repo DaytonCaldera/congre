00e3ae578d8ba641af9e12f8a7b0f4f37941857c
Dayton Caldera <dayton.caldera1711@gmail.com>
2022-09-22 14:01:38 -0600

    Pruebas

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('testing', function () {
+    echo "Testing my own artisan command";
+})->purpose('Test artisan custom command');
